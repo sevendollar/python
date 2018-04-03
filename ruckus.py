141b58e8b70f2ed55f2591e1916aef9508becf93
sevendollar <sevendollar@gmail.com>
2018-04-03 08:40:33 +0000

    first added get_acls method

--- ruckus.py
+++ ruckus.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 import os
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import re
 
 # profile = webdriver.FirefoxProfile()
 # profile.default_preferences['webdriver_assume_untrusted_issuer'] = False
@@ -12,18 +16,16 @@ import os
 # options.add_argument('--ignore-certificate-errors')
 # driver = webdriver.Chrome(chrome_options=options)
 
-class Ruckus:
-    import time
-    from selenium import webdriver
-    from bs4 import BeautifulSoup
 
+class Ruckus:
     logon_url = 'https://wifi.cvl.com.tw/admin/login.jsp'
     dashboard_url = 'https://wifi.cvl.com.tw/admin/dashboard.jsp'
     conf_acl_url = 'https://wifi.cvl.com.tw/admin/conf_acls.jsp'
     acl = 'edit-acl-2'
+    acls = None
 
     def __init__(self, username, password):
-        self.driver = self.__class__.webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
+        self.driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
         self.driver.implicitly_wait(30)
         self.driver.set_window_size(1024, 768)
 
@@ -31,7 +33,9 @@ class Ruckus:
         self.driver.find_element_by_css_selector('[name="username"]').send_keys(username)
         self.driver.find_element_by_css_selector('[name="password"]').send_keys(password)
         self.driver.find_element_by_css_selector('[name="ok"]').click()
-        self.__class__.time.sleep(3)
+        time.sleep(3)  # wait for the authentication page to be loaded.
+        self.get_acls()
+
 
     def add_mac(self, mac):
         if not self.exist_mac(mac):
@@ -45,6 +49,7 @@ class Ruckus:
             return 'mac existed.'
 
     def remove_mac(self, mac):
+        # todo remove the specified mac.
         if self.exist_mac(mac):
             pass
             return 'success remove the mac.'
@@ -59,19 +64,27 @@ class Ruckus:
         return tuple(macs.replace(' delete', '').replace('\n', ' ').lower().split(' '))
 
     def exist_mac(self, mac):
-        if mac in self.driver.page_source:
+        if mac in self.all_mac():
             return True
         return False
 
+    def get_acls(self):
+        self.driver.get('https://wifi.cvl.com.tw/admin/conf_acls.jsp')
+        acl_objs = self.driver.find_element_by_css_selector('table#acl.listTable > tbody').find_elements_by_css_selector('td.action > span')  # get acl lists
+        self.__class__.acls = tuple([i.get_attribute('id') for i in acl_objs if 'edit' in i.get_attribute('id')])  # filter out the acl
+        return self.__class__.acls
+
     def __del__(self):
-        return self.driver.close()
+        return self.driver.quit()
 
 
 if __name__ == '__main__':
     user = os.environ.get('RUCKUS_USER')
     pw = os.environ.get('RUCKUS_PASS')
+
     r1 = Ruckus(user, pw)
-    mac = 'bb:bb:bb:bb:bb:bb'
-    print(r1.all_mac())
+    x = r1.get_acls()
+    print(x)
+
     del r1
 
