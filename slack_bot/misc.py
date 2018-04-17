ddb1e9308b757beec96a4e8b6d9d621556437e53
sevendollar <sevendollar@gmail.com>
2018-04-17 09:34:54 +0000

    redefinded return values

--- slack_bot/misc.py
+++ slack_bot/misc.py
@@ -1,3 +1,5 @@
+#!/bin/sh/python
+
 import re
 
 
@@ -12,9 +14,29 @@ def parse_user_words_v2(words):
     chinese_matches = re.finditer(CHINESE_CHARACTER_REGEX, words)
     bad_words_matches = re.finditer(BAD_WORDS_REGEX, words, re.IGNORECASE)
 
-    macs = tuple(m[0] for m in mac_matches)
-    ids_ = tuple(i[0] for i in id_matches)
     chinese_characters = tuple(c[0] for c in chinese_matches)
-    bad_words = tuple(b[0] for b in bad_words_matches)
+    new_words = {
+        'macs': tuple(m[0] for m in mac_matches),
+        'chinese_characters': chinese_characters,
+        'team_name': chinese_characters[0],
+        'team_user': chinese_characters[1],
+        'customer_name': chinese_characters[2],
+        'customer_id': tuple(i[0] for i in id_matches),
+        'bad_words': tuple(b[0] for b in bad_words_matches)
+    }
+    return new_words
 
-    return {'macs': macs, 'ids': ids_, 'chinese_characters': chinese_characters, 'bad_words': bad_words}
+
+def main():
+    x = parse_user_words_v2()
+    print(x.get('team_name', None))
+    print(x.get('team_user', None))
+    print(x.get('customer_name', None))
+    print(x.get('customer_id', None))
+    print(x.get('macs', None))
+    print()
+    print(x.get('bad_words', None))
+
+
+if __name__ == '__main__':
+    main()
